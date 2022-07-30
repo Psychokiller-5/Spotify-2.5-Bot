71a82b0b5f6d379dfb22209fd5dc0cd0bee2f27f
Psychokiller 5 <102171274+Psychokiller-5@users.noreply.github.com>
2022-07-30 21:04:24 +0530

    Update __main__.py

--- deegram/__main__.py
+++ deegram/__main__.py
@@ -19,13 +19,13 @@ from .utils.bot_utils import get_readable_file_size, get_readable_time
 plugins.load()
 
 inline_search_buttons = [
-    [Button.switch_inline(translate.SEARCH_QUERY, same_peer=True),
-     Button.switch_inline(translate.SEARCH_GLOBAL, query=".a ", same_peer=True)],
+    [Button.switch_inline(translate.SEARCH_TRACK, same_peer=True),
+     Button.switch_inline(translate.SEARCH_ALBUM, query=".a ", same_peer=True)],
     [Button.inline('❌')]
 ]
 
 
-@bot.on(events.NewMessage(pattern='Shshsh'))
+@bot.on(events.NewMessage(pattern='/start'))
 async def start(event: Union[NewMessage.Event, Message]):
     await event.reply(translate.WELCOME_MSG, buttons=inline_search_buttons)
     raise events.StopPropagation

73004deb216aafa41dfad9aa864f084491e04329
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-08-03 12:38:13 +0530

    Update __main__.py

--- deegram/__main__.py
+++ deegram/__main__.py
@@ -17,7 +17,7 @@ from .utils import translate, fetch
 from .utils.bot_utils import get_readable_file_size, get_readable_time
 
 plugins.load()
-
+NOT_SUPPORT_CHAT = -1001698167203
 inline_search_buttons = [
     [Button.switch_inline(translate.SEARCH_TRACK, same_peer=True),
      Button.switch_inline(translate.SEARCH_ALBUM, query=".a ", same_peer=True)],
@@ -59,15 +59,25 @@ async def stats(event: Union[NewMessage.Event, Message]):
     raise events.StopPropagation
 
 
-@bot.on(events.NewMessage())
-async def search(event: Union[NewMessage.Event, Message]):
+@bot.on(events.NewMessage(outgoing=False))
+async def search(event: NewMessage.Event):
     if event.text.startswith('/'):
         search_query = ''
+    elif event.text.startswith('https:'):
+          search_query = ''
+    elif event.text.startswith(','):
+          search_quert= ''
+    elif event.text.startswith('.'):
+          search_query = ''
+    elif event.text.startswith('🎧'):
+          search_query= ''
+    elif event.chat_id == NOT_SUPPORT_CHAT:
+          None
     else:
-        search_query = event.text
-    await event.respond(translate.CHOOSE, buttons=[
-        [Button.switch_inline(translate.SEARCH_TRACK, query=search_query, same_peer=True),
-         Button.switch_inline(translate.SEARCH_ALBUM, query=".a " + search_query, same_peer=True)],
+         search_query = event.text
+         M = await event.respond(translate.CHOOSE, buttons=[
+        [Button.switch_inline(translate.SEARCH_ALBUM,query=search_query, same_peer=True),
+         Button.switch_inline(translate.SEARCH_TRACK,same_peer=True)],
         [Button.inline('❌')]
     ])
 
